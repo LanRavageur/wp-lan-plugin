883ee95f04d72908f4030524ee60f73743211540
Francis <francislr@gmail.com>
2012-02-27 11:06:24 -0500

    Added plugin directory

---
+++ lan-org/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// This file is intentionally left blank.
+
+?>
---
+++ lan-org/lan-org.php
@@ -0,0 +1,10 @@
+<?php
+
+// Plugin Name: lan-org
+// Plugin URI:  http://lanravageur.github.com/wp-lan-plugin
+// Description: Wordpress LAN plugin is used to organize LAN Party.
+// Version:     0.0
+// Text Domain: lanorg
+// Domain Path: /lan-org/
+
+?>
