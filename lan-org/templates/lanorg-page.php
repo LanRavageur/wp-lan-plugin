e6d15cde14984d35c8374a7a02ce68538a9d4dad
Francis <francislr@gmail.com>
2012-03-11 22:27:49 -0400

    Added default custom page template

--- lan-org/templates/lanorg-page.php
+++ lan-org/templates/lanorg-page.php
@@ -1,1 +1,12 @@
-ABCDEF
+<?php
+get_header();
+
+wp_enqueue_style('lanorg-form');
+
+?>
+<div id="primary">
+<div id="content" role="main">
+<?php echo the_content(); ?>
+</div>
+</div>
+<?php get_footer(); ?>
