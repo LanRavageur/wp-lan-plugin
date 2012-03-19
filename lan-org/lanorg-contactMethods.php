4749906fb671a1fa73bfe4a531fdff7df6e476dd
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2012-03-19 12:43:06 -0400

    Rearrenged order for display in admin profile panel, changed ids to match those from custom profile

--- lan-org/lanorg-contactMethods.php
+++ lan-org/lanorg-contactMethods.php
@@ -9,11 +9,15 @@ function my_user_contactmethods($user_contactmethods){
 	unset($user_contactmethods['jabber']);
 
 	// Adds some new cool contact methods :)
-	$user_contactmethods['twitter'] = __('Twitter Username', 'lanorg') . ' <span class="description">' . __('(without @)', 'lanorg') . '</span>';
-	$user_contactmethods['facebook'] = __('Facebook Username', 'lanorg');
-	$user_contactmethods['steam'] = __('Steam Account', 'lanorg') . ' <span class="description">' . __('(to let new friends meet you ingame)', 'lanorg') . '</span>';
-	$user_contactmethods['origin'] = __('Origin Account', 'lanorg') . ' <span class="description">' . __('(same here)', 'lanorg') . '</span>';
-
+	$user_contactmethods['steam_id'] = __('Steam Account', 'lanorg') . ' <span class="description">' . __('(to let new friends meet you ingame)', 'lanorg') . '</span>';
+	$user_contactmethods['origin_id'] = __('Origin Account', 'lanorg') . ' <span class="description">' . __('(same here)', 'lanorg') . '</span>';
+	$user_contactmethods['battlefield3_id'] = __('Battlefield 3 Soldier', 'lanorg') . ' <span class="description">' . __('(same here)', 'lanorg') . '</span>';
+	$user_contactmethods['battlenet_id'] = __('Battle.net Account', 'lanorg') . ' <span class="description">' . __('(same here)', 'lanorg') . '</span>';
+	
+	$user_contactmethods['facebook_id'] = __('Facebook Username', 'lanorg');
+	$user_contactmethods['twitter_id'] = __('Twitter Username', 'lanorg') . ' <span class="description">' . __('(without @)', 'lanorg') . '</span>';
+	$user_contactmethods['youtube_id'] = __('Youtube Account', 'lanorg');
+	
 	return $user_contactmethods;
 }
 ?>
