60b487952db90d4e1dd2e7e72a184b8360ec94d6
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2012-03-19 21:28:44 -0400

    updated translations

--- lan-org/templates/lanorg-registration.php
+++ lan-org/templates/lanorg-registration.php
@@ -3,14 +3,14 @@ global $lanOrg, $page_title;
 
 $lanOrg->require_login();
 
-$page_title = 'Admission';
+$page_title = __('Admission', 'lanorg');
 
 ?>
-<h1 style="clear: none;" class="entry-title">Confirmer sa présence</h1>
-<p>Choisissez les événments que vous désirez participer.</p>
+<h1 style="clear: none;" class="entry-title"><?php _e('Confirm check in', 'lanorg'); ?></h1>
+<p><?php _e('Choose the event you wish to participate', 'lanorg'); ?></p>
 <form class="lanorg-form" method="POST">
 <?php echo lanorg_get_registration_form_markup(); ?>
 <p>
-<input type="submit" class="lanorg-button" value="S'inscrire à l'événement" />
+<input type="submit" class="lanorg-button" value="<?php _e('Register', 'lanorg'); ?>" />
 </p>
 </form>
