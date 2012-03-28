9e8ef80d95339aa73ca26f4ec7402b4c5d8bc887
J:-D Pearson <jeandanielpearson@gmail.com>
2012-03-27 21:11:41 -0400

    better profile alpha0,11
    adding english traduction for 2 lines of profile

--- lan-org/templates/lanorg-profile.php
+++ lan-org/templates/lanorg-profile.php
@@ -7,15 +7,26 @@ $lanOrg->require_login();
 $user = wp_get_current_user();
 
 if ($user_info) {
-	$page_title = 'Profile de ' . $user_info->data->user_login;
+	$page_title = __('Profile of ', 'lanorg') . $user_info->data->user_login;
 }
 else {
 	$page_title = 'Profile inexistant';
 }
 
 ?>
-<h1 style="clear: none;" class="entry-title">Profile de <?php echo $user_info->data->user_login; ?></h1>
-<?php echo get_avatar($user_info->data->ID); ?>
+<h1 style="clear: none;" class="entry-title"><?php echo __('Profile of ', 'lanorg') . $user_info->data->user_login; ?></h1>
+
+<?php 
+	echo get_avatar($user_info->data->ID);
+	$fullName = $user_info->user_firstname;
+	if ($fullName <> ''){
+		$fullName .= ', ';
+	}
+	$fullName .= $user_info->user_lastname;
+	if ($fullName <> ''){
+		echo '<h3>' . __('Also known as ', 'lanorg') . $fullName . '</h3>';
+	}
+?>
 <dl>
 <?php foreach ($user_meta_data as $key => $value) { ?>
 <dt><?php echo htmlentities($key, NULL, 'UTF-8'); ?></dt>
